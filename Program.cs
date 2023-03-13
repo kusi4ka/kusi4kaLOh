2cbad47cd0a9aaad9fdb1c0f98f16e6abbdf11eb
kusi4ka <ghjejdndnx@gmail.com>
2023-03-13 16:56:58 +0300

    First commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+
+
+namespace B3
+{
+    class Firma
+    {
+       public string Name { get; set; }
+       public string Adress { get; set; }
+       public int Year { get; set; }
+        public Firma(string name, string adress, int year) 
+        {
+            Name = name;
+            Adress = adress;
+            Year = year;
+        }
+    }
+    internal class Program 
+    {
+        static void Main(string[] args)
+        {
+
+            Firma firma = new Firma("Kuskus", "molodezhnaya", 2005);
+
+            // Выводим информацию об изделии
+            Console.WriteLine("Name: " + firma.Name);
+            Console.WriteLine("Adress: " + firma.Adress);
+            Console.WriteLine("Year: " + firma.Year);
+            Console.ReadKey();
+        }
+    }
+}
